92513ab255f83bd1ce2a6ab37ee6063d97564be3
Long Du <long020396@gmail.com>
2019-05-16 17:05:06 +0700

    Finish lecture 106

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import classes from './App.module.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import withClass from '../hoc/withClass.js';
+import Aux from '../hoc/Auxiliary';
 
 class App extends Component {
   constructor(props) {
@@ -17,7 +19,8 @@ class App extends Component {
     ],
     otherState: 'Some other value',
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -62,7 +65,12 @@ class App extends Component {
     persons[personIndex] = person;
 
 
-    this.setState({ persons: persons });
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1
+      }
+    });
   }
 
   togglePersonsHandler = () => {
@@ -83,7 +91,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <Aux>
         <button
           onClick={() => {
             this.setState({
@@ -100,10 +108,10 @@ class App extends Component {
             clicked={this.togglePersonsHandler} />
         ) : null}
         {persons}
-      </div>
+      </Aux>
     );
   }
   // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
 }
 
-export default App;
+export default withClass(App, classes.App);
